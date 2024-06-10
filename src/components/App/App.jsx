459150dcdf924d66db81cbe4145e5f4a666c9420
Nickolai <lindeman072@mail.ru>
2024-06-10 15:55:40 +0400

    add: days2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,15 @@
 import './App.css';
 import Main from '../Main/Main';
-// import * as Api from './../servises/requests'
-// import { useState } from 'react';
-// import { useEffect } from 'react';
+import * as Api from './../servises/requests'
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 export default function App() {
-// const [weather, setWeather] = useState([])
-// const [forecast, setForecast] = useState([])
-// console.log('forecast: ', forecast);
-// console.log('weather: ', weather);
-// const location = 'Ulyanovsk'
-// const lang = 'Ru'
-// const days = 7
+const [weather, setWeather] = useState([])
+const [forecast, setForecast] = useState([])
+const location = 'Ulyanovsk'
+const lang = 'Ru'
+const days = 1
 
 // useEffect(() => {
 // Api.getWeather(location, lang)
@@ -24,20 +22,20 @@ export default function App() {
 // }, [location, lang])
 
 
-// useEffect(() => {
-//   Api.getForecast(location, days, lang)
-//   .then((data) => {
-//     setWeather(data.current)
-//     setForecast(data.forecast)
-//   })
-//   .catch((error) => {
-//     console.error(error)
-//   })
-//   }, [location, days, lang])
+useEffect(() => {
+  Api.getForecast(location, days, lang)
+  .then((data) => {
+    setWeather(data)
+    setForecast(data.forecast)
+  })
+  .catch((error) => {
+    console.error(error)
+  })
+  }, [location, days, lang])
 
 return (
     <>
-      <Main />
+      <Main weather={weather} forecast={forecast}/>
     </>
   );
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,20 +1,27 @@
 import './Main.css';
-import MainPicture from './../../assets/Image1.png';
 import Chart from './Chart/Chart';
 import ContainerDays from './ContainerDays/ContainerDays';
 
-export default function Main() {
+export default function Main({ weather, forecast }) {
+  console.log('weather: ', forecast);
+
+  console.log('weather: ', forecast.forecastday?.hour);
+
   return (
     <div className='main'>
       <div className='main__container-city'>
-        <p className='main__city'>San Fransisco</p>
-        <p className='main__time'>11:00</p>
+        <p className='main__city'>{weather.location?.name}</p>
+        <p className='main__time'>{weather.location?.localtime}</p>
       </div>
 
       <div>
-        <img className='main__image' src={MainPicture} alt='Иконка картинки' />
-        <p className="main__weather">thunderstorm</p>
-        <p className="main__degress">24</p>
+        <img
+          className='main__image'
+          src={weather.current?.condition.icon}
+          alt='Иконка картинки'
+        />
+        <p className='main__weather'>{weather.current?.condition.text}</p>
+        <p className='main__degress'>{weather.current?.temp_c}°</p>
       </div>
       <Chart />
       <ContainerDays />
